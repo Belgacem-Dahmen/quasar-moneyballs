e53eb8e737a26fa39650880a736f0d9281f7ee4a
belgacem-dahmen <belgacem.dahmen@gmail.com>
2024-10-05 19:07:34 +0100

    feature : user can navigate between pages using NavLink component

---
+++ src/components/Nav/NavLink.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-item clickable class="text-white" tag="a" :to="link">
+    <q-item-section v-if="icon" avatar>
+      <q-icon :name="icon" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label>{{ title }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script setup>
+const { title, link, icon } = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+
+  link: {
+    type: String,
+    default: "#",
+  },
+
+  icon: {
+    type: String,
+    default: "",
+  },
+});
+</script>
