2390ed8f5737cfc8d354ab8f25efbe0bd01770a8
Belgacem-Dahmen <belgacem.dahmen@gmail.comn>
2024-10-07 17:56:15 +0200

    Merging UseNotify

--- src/use/UseNotify.js
+++ src/use/UseNotify.js
@@ -35,28 +35,3 @@ const successNotification = {
       message : message
     });
   };
-=======
-
-errorNotification = {
-    color: "red-5",
-    textColor: "white",
-    icon: "warning",
-    message: "You need to accept the license and terms first",
-  }
-successNotification = {
-    color: "green-4",
-    textColor: "white",
-    icon: "cloud_done",
-    message: "Vos informations ont été mises à jour",
-  }
-  
-  
-  export const useSuccessNotification =  () => {
-    $q.notify(successNotification);
-}
-
-
-export const useErrorNotification =  () => {
-    $q.notify(errorNotification);
-}
->>>>>>> dfe824c577121bde6b15eefc4e951ec6ffc78d32
